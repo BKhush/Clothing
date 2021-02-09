a79695b8b259fe4e156175b9861590e8c23a8fd7
BKhush <khushpreetbuttar@gmail.com>
2021-02-08 19:32:55 -0800

    React Router setup

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
-import Homepage from './pages/homepage/homepage.component'
+import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
-  return <div>
-  <Homepage/>
-  </div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
