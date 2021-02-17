61d09794eb065c037a0c7fdb246123fabd533db2
BKhush <khushpreetbuttar@gmail.com>
2021-02-16 20:14:29 -0800

    added shop preview into its own component (collections overview)

--- crwn-clothing/src/pages/shop/shop.component.jsx
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import CollectionsOverView from '../../components/collections-overview/collections-overview.component'
 
-import CollectionPreview from "../../components/collection-preview/collection-preview";
 
-import { selectCollections } from "../../redux/shop/shop.selectors";
 
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+  <CollectionsOverView/>
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
