f3d182789d6e7fba0758ad332d98093917e743b8
BKhush <khushpreetbuttar@gmail.com>
2021-02-16 20:05:09 -0800

    Collection state into redux for shop data

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
@@ -16,9 +16,8 @@ const Directory = ({ sections }) => (
   </div>
 );
 
-
 const mapStateToProps = createStructuredSelector({
-  sections: selectDirectorySections
+  sections: selectDirectorySections,
 });
 
 export default connect(mapStateToProps)(Directory);
