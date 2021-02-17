29b97671f083ba0b0a07f2eeb89cbeefe64a2414
BKhush <khushpreetbuttar@gmail.com>
2021-02-16 21:38:58 -0800

    routing for collections

--- crwn-clothing/src/pages/shop/shop.component.jsx
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
-import CollectionsOverView from '../../components/collections-overview/collections-overview.component'
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-
-
-const ShopPage = ({ collections }) => (
+const ShopPage = ({match}) => (
   <div className="shop-page">
-  <CollectionsOverView/>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
